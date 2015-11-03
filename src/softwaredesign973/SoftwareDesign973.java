76ffa5745a1becd33b6a3e0c08cb52c5904fcfcf
Jacob Sawatzky <jsawatzky@outlook.com>
2015-11-03 09:10:06 -0500

    Initialized the NetBeans Project
    Set up the NetBeans Project

---
+++ src/softwaredesign973/SoftwareDesign973.java
@@ -0,0 +1,15 @@
+package softwaredesign973;
+
+/**
+ * @author Jacob Sawatzky
+ * @author David Park
+ * @author Julian Li
+ */
+public class SoftwareDesign973 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("NOT YET COMPLETED!!!");
+        
+    }
+}
