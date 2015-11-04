1f182e97eaa34db97e5420fd654f9a27d7129110
Jacob Sawatzky <jsawatzky@outlook.com>
2015-11-04 17:41:23 -0500

    Finish the Expression constructor
    Made the Expression constructor properly separate the string into terms.

--- src/softwaredesign973/polynomial/Expression.java
+++ src/softwaredesign973/polynomial/Expression.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class Expression implements PolynomialCompenent {
     
+    public boolean valid = true;
+    
     private ArrayList<Term> terms = new ArrayList<>();
     
     public Expression(String e) {
@@ -12,7 +14,22 @@ public class Expression implements PolynomialCompenent {
             e = "+" + e;
         }
         
+        for (int i = 0; i < e.length() - e.replace("+", "").replace("-", "").length(); i++) {
+            
+            int index = Math.min(e.substring(1).index("+"), e.substring(1).index("-"));
+            
+            terms.append(new Term(e.substring(0, index)));
+            
+            e = e.substring(index);
+            
+        }
         
+        for (Term t: terms) {
+            if (!t.valid) {
+                valid = false;
+                break;
+            }
+        }
         
     }
     
